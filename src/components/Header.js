e16c30c8d1b83939796134807696e3171f839713
Kevin Browne <kbrowne97@gmail.com>
2022-11-26 20:22:27 -0500

    adds css reset and header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.page_header}>
+      <h1>CV-Maker</h1>
+    </div>
+  );
+};
+
+export default Header;
