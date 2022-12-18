273c2dfaf7ea300dbb237a13cf10862c51404873
Kevin Browne <kbrowne97@gmail.com>
2022-12-17 21:02:32 -0500

    remove commented code.  remove unused index parameter

--- src/components/EducationInfo.js
+++ src/components/EducationInfo.js
@@ -8,7 +8,7 @@ const EducationInfo = ({
 }) => {
   return (
     <>
-      {educationArr.map((education, index) => {
+      {educationArr.map((education) => {
         return (
           <section className={styles.form_section} key={education.uniqId}>
             <h2 className={styles.education_info}>Education</h2>
@@ -49,54 +49,5 @@ const EducationInfo = ({
     </>
   );
 };
-
-//   console.log(educationArr);
-//   if (educationArr.length === 0) {
-//     return (
-//       <section className={styles.form_section}>
-//         <button className={styles.add_btn}>Add Education</button>
-//       </section>
-//     );
-//   } else {
-//     return educationArr.map((education, index) => {
-//       return (
-//         <section className={styles.form_section} key={education.uniqId}>
-//           <h2 className={styles.education_info}>Education</h2>
-//           <input
-//             placeholder="School Name"
-//             value={education.school}
-//             name="school"
-//             onChange={(e) => handleEducationInput(e, education.uniqId)}
-//           ></input>
-//           <input
-//             placeholder="Major"
-//             value={education.major}
-//             name="major"
-//             onChange={(e) => handleEducationInput(e, education.uniqId)}
-//           ></input>
-//           <input
-//             placeholder="Start Date"
-//             value={education.startDate}
-//             name="startDate"
-//             onChange={(e) => handleEducationInput(e, education.uniqId)}
-//           ></input>
-//           <input
-//             placeholder="End Date"
-//             value={education.endDate}
-//             name="endDate"
-//             onChange={(e) => handleEducationInput(e, education.uniqId)}
-//           ></input>
-//           <button
-//             type="button"
-//             className={styles.add_btn}
-//             onClick={handleAddEducation}
-//           >
-//             Add Education
-//           </button>
-//         </section>
-//       );
-//     });
-//   }
-// };
 
 export default EducationInfo;
