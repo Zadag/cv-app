4627b12c2753241ff2091756aefa6eb877de5def
Kevin Browne <kbrowne97@gmail.com>
2022-12-21 14:57:34 -0500

    replace svg files with fontawesome imports. Rearrange line break and objective tags

--- src/components/ResumePreview.js
+++ src/components/ResumePreview.js
@@ -1,6 +1,11 @@
 import React from "react";
 import styles from "./ResumePreview.module.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faLocationDot,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 class ResumePreview extends React.Component {
   constructor(props) {
     super(props);
@@ -15,17 +20,26 @@ class ResumePreview extends React.Component {
           </h1>
           <div className={styles.user_details_container}>
             <div className={styles.user_detail}>
+              <FontAwesomeIcon className={styles.svg_icon} icon={faPhone} />
               <p>{this.props.contactValues.phoneNumber}</p>
             </div>
             <div className={styles.user_detail}>
+              <FontAwesomeIcon className={styles.svg_icon} icon={faEnvelope} />
               <p>{this.props.contactValues.email}</p>
             </div>
             <div className={styles.user_detail}>
+              <FontAwesomeIcon
+                className={styles.svg_icon}
+                icon={faLocationDot}
+              />
               <p>{this.props.contactValues.location}</p>
             </div>
           </div>
+          <div className={styles.line_break}></div>
+          <p className={styles.user_objective}>
+            {this.props.contactValues.objective}
+          </p>
         </div>
-        <div className={styles.line_break}></div>
       </div>
     );
   }
